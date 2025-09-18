159489cd24f88b9479c33e19286933a03bb7fdc7
Tilda-Eng <sngolinskiy@gmail.com>
2025-09-18 13:47:00 +0300

    Update Clear_setup.sh

--- Clear_setup.sh
+++ Clear_setup.sh
@@ -11,7 +11,7 @@ sudo apt update
 sudo apt upgrade -y
 
 header "Installing Python and tools"
-sudo apt install -y python3 python3-pip python3-venv
+sudo apt install -y python3 python3-pip python3-venv git
 
 header "Installing GPIO and Serial libraries"
 sudo apt install -y python3-rpi-lgpio python3-serial
